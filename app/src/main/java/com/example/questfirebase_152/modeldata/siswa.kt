f27bcc0f4b38072eff12cbdf0a5df7ad58e78bfb
muhammadrayhanwidayat <murayat99@gmail.com>
2026-01-02 21:29:01 +0700

    data class

--- app/src/main/java/com/example/questfirebase_152/modeldata/siswa.kt
+++ app/src/main/java/com/example/questfirebase_152/modeldata/siswa.kt
@@ -1,2 +1,17 @@
 package com.example.questfirebase_152.modeldata
 
+/**
+ * Model data yang merepresentasikan entitas Siswa.
+ *
+ * @property id ID unik siswa (Long)
+ * @property nama Nama lengkap siswa
+ * @property alamat Alamat tempat tinggal siswa
+ * @property telpon Nomor telepon siswa
+ */
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
