481cde8c5fdf4c7cc40882ac3da453f53ec8eebc
muhammadrayhanwidayat <murayat99@gmail.com>
2026-01-09 17:39:57 +0700

    detailviewmodel

--- app/src/main/java/com/example/questfirebase_152/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_152/viewmodel/DetailViewModel.kt
@@ -1,2 +1,23 @@
+@file:OptIn(InternalSerializationApi::class)
 package com.example.questfirebase_152.viewmodel
+
+
+import com.example.questfirebase_152.modeldata.Siswa
+import kotlinx.serialization.InternalSerializationApi
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.questfirebase_152.repositori.RepositorySiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.questfirebase_152.view.route.DestinasiDetail
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
 
