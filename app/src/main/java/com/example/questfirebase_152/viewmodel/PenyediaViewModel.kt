739f82b3b5e012a03e94d403026ad53620f1a793
muhammadrayhanwidayat <murayat99@gmail.com>
2026-01-09 18:07:53 +0700

    penyediaviewmodel v 2

--- app/src/main/java/com/example/questfirebase_152/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questfirebase_152/viewmodel/PenyediaViewModel.kt
@@ -1,22 +1,37 @@
 package com.example.questfirebase_152.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questfirebase_152.repositori.AplikasiDataSiswa
 
-fun CreationExtras.aplikasiDataSiswa() : AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
-        )
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
+            as AplikasiDataSiswa)
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container.repositorySiswa)
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
         }
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container.repositorySiswa)
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa
+            )
         }
     }
 }
